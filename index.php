f0c98ba245c15980883570bcfef42f995d7052aa
Erme <ermelindo.schultz@gmail.com>
2020-04-30 14:43:21 -0300

    Inclusao da pagina inicial e sua estilizacao

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Gerenciador de Produtos</title>
+    </head>
+    <body>
+        <div class="container">
+            <div id="logo_inicial" class="row">
+                Gerenciador de Produtos
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <a href="produtos.php" class="card text-center">
+                        <svg class="bi bi-collection-fill icone_inicial" width="10em" height="10em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <rect width="16" height="10" rx="1.5" transform="matrix(1 0 0 -1 0 14.5)"/>
+                            <path fill-rule="evenodd" d="M2 3a.5.5 0 00.5.5h11a.5.5 0 000-1h-11A.5.5 0 002 3zm2-2a.5.5 0 00.5.5h7a.5.5 0 000-1h-7A.5.5 0 004 1z" clip-rule="evenodd"/>
+                        </svg>
+                        <div class="card-body">
+                            <h5 class="card-title">Produtos</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm">
+                    <a href="vendedores.php" class="card text-center">
+                        <svg class="bi bi-people-fill icone_inicial" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 100-6 3 3 0 000 6zm-5.784 6A2.238 2.238 0 015 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 005 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" clip-rule="evenodd"/>
+                        </svg>
+                        <div class="card-body">
+                            <h5 class="card-title">Vendedores</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm">
+                    <a href="vendas.php" class="card text-center">
+                        <svg class="bi bi-briefcase-fill icone_inicial" width="10em" height="10em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M0 12.5A1.5 1.5 0 001.5 14h13a1.5 1.5 0 001.5-1.5V6.85L8.129 8.947a.5.5 0 01-.258 0L0 6.85v5.65z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M0 4.5A1.5 1.5 0 011.5 3h13A1.5 1.5 0 0116 4.5v1.384l-7.614 2.03a1.5 1.5 0 01-.772 0L0 5.884V4.5zm5-2A1.5 1.5 0 016.5 1h3A1.5 1.5 0 0111 2.5V3h-1v-.5a.5.5 0 00-.5-.5h-3a.5.5 0 00-.5.5V3H5v-.5z" clip-rule="evenodd"/>
+                        </svg>
+                        <div class="card-body">
+                            <h5 class="card-title">Vendas</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm">
+                    <a href="relatorios.php" class="card text-center">
+                        <svg class="bi bi-graph-up icone_inicial" width="10em" height="10em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 0h1v16H0V0zm1 15h15v1H1v-1z"/>
+                            <path fill-rule="evenodd" d="M14.39 4.312L10.041 9.75 7 6.707l-3.646 3.647-.708-.708L7 5.293 9.959 8.25l3.65-4.563.781.624z" clip-rule="evenodd"/>
+                            <path fill-rule="evenodd" d="M10 3.5a.5.5 0 01.5-.5h4a.5.5 0 01.5.5v4a.5.5 0 01-1 0V4h-3.5a.5.5 0 01-.5-.5z" clip-rule="evenodd"/>
+                        </svg>
+                        <div class="card-body">
+                            <h5 class="card-title">Relatórios</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
