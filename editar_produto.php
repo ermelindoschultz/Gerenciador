490ccc05e4c1fd81085b6856e6c38bbaa7386897
Erme <ermelindo.schultz@gmail.com>
2020-04-30 22:26:30 -0300

    Prototipos das paginas de adiçao e ediçao de produtos e vendedores finalizados

---
+++ editar_produto.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <meta charset="utf-8" />
+        <title>Gerenciador de Produtos</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <a class="navbar-brand" href="index.php">Gerenciador de Produtos</a>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="produtos.php">Produtos</a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link" href="vendedores.php">Vendedores</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="vendas.php">Vendas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="relatorios.php">Relatórios</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm text-center">
+                    <h1>Adicionar informações de produto</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+        <form>
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" id="nome">
+            </div>
+            <div class="form-group">
+                <label for="sobrenome">Valor</label>
+                <input type="number" class="form-control" id="valor">
+            </div>
+            <button type="submit" class="btn btn-primary">Editar</button>
+            <button type="button" class="btn btn-danger" onclick="location.href = 'produtos.php'">Cancelar</button>
+        </form>
+        </div>
+    </body>
+</html>
